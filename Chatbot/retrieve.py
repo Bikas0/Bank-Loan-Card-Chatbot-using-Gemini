604a96eedeb5d00fb1951ec6d85bcd768d3a7e9c
Bikas0 <bikasictiu1718@gmail.com>
2025-12-28 11:49:03 +0600

    Update the library and add requirements file

--- Chatbot/retrieve.py
+++ Chatbot/retrieve.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 
 # Embedding client (same as indexing)
-from embed_to_faiss import GeminiEmbeddingClient
+from embedding import GeminiEmbeddingClient
 
 # ===============================
 # LOAD ENV
